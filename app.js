d9de9990777f8bcc3ead6049e48d0a953e4c6008
Maria <lu_vinas@hotmail.com>
2022-05-19 22:45:23 -0400

    data display complete, iteration looping

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const data = [
     gender: 'female',
     lookingfor: 'male',
     location: 'Vancouver, BC',
-    image: 'https://randomuser.me/api/portraits/woman/79.jpg'
+    image: 'https://randomuser.me/api/portraits/women/36.jpg'
     },
     {
     name: 'Jill Smith',
@@ -21,27 +21,40 @@ const data = [
     gender: 'female',
     lookingfor: 'male',
     location: 'Calgary, AB',
-    image: 'https://randomuser.me/api/portraits/women/25.jpg'
+    image: 'https://randomuser.me/api/portraits/women/24.jpg'
     }
 ]
 
 const profiles = profileIterator(data);
 
+// call first profile on page load
+nextProfile();
+
 // Next event
 document.getElementById('next').addEventListener('click', nextProfile);
 
-// Next profile functionm
+// Next profile function
 function nextProfile() {
     const currentProfile =profiles.next().value;
 
-    document.getElementById('profileDisplay').innerHTML = `
-    <ul class="list-group">
-        <li class="list-group-item>Name:${currentProfile.name} </li>
-        <li class="list-group-item>Age:${currentProfile.age} </li>
-        <li class="list-group-item>Location:${currentProfile.location} </li>
-        <li class="list-group-item>Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor} </li>
-    </ul>`
+    if(currentProfile !== undefined){
+        document.getElementById('profileDisplay').innerHTML = `<ul class="list-group">
+            <li class="list-group-item">Name: ${currentProfile.name}</li>
+            <li class="list-group-item">Age: ${currentProfile.age}</li>
+            <li class="list-group-item">Location: ${currentProfile.location}</li>
+            <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
+        </ul>`;
+    
+        document.getElementById('imageDisplay').innerHTML = `<img src="${currentProfile.image}">`
+
+    } else {
+        // no more profiles
+        window.location.reload();
+
+    }
 }
+
+
 //Profile iterator
 function profileIterator(profiles) {
     let nextIndex = 0;
